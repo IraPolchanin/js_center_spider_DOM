b70f095c491142dbeb81dfc0d101af6799aa06e2
Iryna Polchaninova <ira.polchanin@gmail.com>
2024-10-16 15:41:57 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const xPosition = wall.offsetWidth / 2 - spider.offsetWidth / 2;
+const yPosition = wall.offsetHeight / 2 - spider.offsetHeight / 2;
+
+spider.style.left = `${xPosition}px`;
+spider.style.top = `${yPosition}px`;
