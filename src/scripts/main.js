472604296eee0a7d21988ee603f0059d703933e4
Iryna Polchaninova <ira.polchanin@gmail.com>
2024-10-16 15:50:38 +0300

    add resizing case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,15 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const xPosition = wall.offsetWidth / 2 - spider.offsetWidth / 2;
-const yPosition = wall.offsetHeight / 2 - spider.offsetHeight / 2;
+function centerSpider() {
+  const xPosition = wall.offsetWidth / 2 - spider.offsetWidth / 2;
+  const yPosition = wall.offsetHeight / 2 - spider.offsetHeight / 2;
 
-spider.style.left = `${xPosition}px`;
-spider.style.top = `${yPosition}px`;
+  spider.style.left = `${xPosition}px`;
+  spider.style.top = `${yPosition}px`;
+}
+
+centerSpider();
+
+window.addEventListener('resize', centerSpider);
+
